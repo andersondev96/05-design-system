994ea8e3ee1ca0baaa5a5aa4c001f3f49ed409c5
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-10-21 21:20:14 -0300

    feat?: Add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+	title: 'Data display/Avatar',
+	component: Avatar,
+
+	args: {
+		src: 'https://github.com/andersondev96.png',
+		alt: 'Anderson Fernandes'
+	},
+	argTypes: {
+		src: {
+			control: {
+				type: 'text',
+			}
+		},
+	},
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+	args: {
+		src: undefined,
+	}
+}
